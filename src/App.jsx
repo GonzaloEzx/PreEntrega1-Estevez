67c9e426734d5689876b106396f0e46a76d17487
Gonzalo M. Estévez <gonzalomartinestevez@gmail.com>
2023-03-23 19:51:57 -0300

    se añádio App

---
+++ src/App.jsx
@@ -0,0 +1,73 @@
+import { useRef, useState } from 'react'
+
+import { ItemCount } from './components/ItemCount/ItemCount'
+import Titulo from './components/Titulo/Titulo'
+import { Formulario } from './components/Formulario/Formulario'
+import './App.css'
+
+let task = (numero1, numero2) => {
+    new Promise((resuelto, reject)=>{
+    // acciones
+        
+    })
+}
+
+
+task(12, 6)
+    .then(resp=>{
+        // throw new Error('Error Forzad')
+        console.log(resp)
+    }, err=>console.log(err))
+    .catch(err => console.log(err))
+
+
+
+function App() { 
+    const divRef = useRef(null)
+    // console.log(divRef)
+
+    const handleClick = () => {
+        divRef.current.innerHTML = 'nuevo contenido'
+    }
+    // console.log('Componente App')
+    return(
+        <div className='App'>
+                <nav>
+                <ul>
+                    <li>
+                    <Link to="/">Inicio</Link>
+                    </li>
+                    <li>
+                    <Link to="/acerca-de">Acerca de</Link>
+                    </li>
+                    <li>
+                    <Link to="/contacto">Contacto</Link>
+                    </li>
+                </ul>
+                </nav>
+
+                    <div className="App">
+                    <Navbar />
+                    {/* El resto del contenido de tu aplicación */}
+                    </div>
+                    
+            {/* <div ref={divRef}>
+                Contenido original
+            </div>
+            <button onClick={handleClick}>Cambiar valor</button>
+            <ItemCount /> */}
+        </div>
+)}
+
+export default App
+
+
+// render 
+// 1 ver -> montaje
+// sgtes re render 
+// última -> dismounting
+
+// que produce un re render : 
+    // cambios en un props
+    // cambios en un estados
+    // - evento
